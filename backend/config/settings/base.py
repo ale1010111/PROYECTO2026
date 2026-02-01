08d7d89022d0e9644cd7a52e4391c7b0f5b8d834
ale1010111 <alejandro71133089mail.com>
2026-01-31 22:44:55 -0400

    RESTFRAMEWORK

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 
 from django.urls import path, include
 from django.contrib import admin
